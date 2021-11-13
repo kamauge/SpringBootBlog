2b977a1b0527d2ea03132265fc73213dcd2e2283
Gerald.Kamau <gerald.kamau@imbank.co.ke>
2021-11-13 14:24:58 +0300

    created post entity,repository and exception class

---
+++ src/main/java/com/example/blog/entity/Post.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.blog.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Gerald.Kamau
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table( name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title",nullable=false)
+    private String title;
+    @Column(name = "description",nullable=false)
+    private String description;
+    @Column(name = "content",nullable=false)
+    private String content;
+    
+    
+}
